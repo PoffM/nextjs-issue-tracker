c9819c7b071de03b5de88f4c18eb94e17f33118e
Mathew Poff <matp@live.ca>
2022-08-25 14:51:53 -0400

    Color mode code: * Added comments, * Fixed warning * Fixed white flicker

--- src/utils/color-mode/ColorModeApplier.tsx
+++ src/utils/color-mode/ColorModeApplier.tsx
@@ -1,10 +1,21 @@
 import { useLayoutEffect } from "react";
 import { useColorMode } from "./useColorMode";
 
+/**
+ * Layout effect that only runs in the browser.
+ * Avoids the warning log from Next.js.
+ */
+const useBrowserLayoutEffect =
+  typeof window !== "undefined" ? useLayoutEffect : () => undefined;
+
+/**
+ * Appllies the color mode from localstorage to the document element.
+ * Needed for Tailwind and DaisyUI.
+ */
 export function ColorModeApplier() {
   const { colorMode } = useColorMode();
 
-  useLayoutEffect(() => {
+  useBrowserLayoutEffect(() => {
     document.documentElement.classList.remove("light", "dark");
     document.documentElement.classList.add(colorMode);
     document.documentElement.setAttribute("data-theme", colorMode);
--- src/utils/color-mode/useColorMode.ts
+++ src/utils/color-mode/useColorMode.ts
@@ -3,11 +3,10 @@ import useLocalStorage from "@rehooks/local-storage";
 export function useColorMode() {
   const initialColorMode =
     typeof window === "object"
-      ? localStorage?.getItem("color-mode") ??
-        (window.matchMedia?.("(prefers-color-scheme: dark)").matches
-          ? "dark"
-          : "light")
-      : "light";
+      ? window.matchMedia?.("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light"
+      : "dark";
 
   const [colorMode, setColorMode] = useLocalStorage(
     "color-mode",
