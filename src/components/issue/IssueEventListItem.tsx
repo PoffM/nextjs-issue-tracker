f69513bb175f76ec1aa0941d5de64356762ac6ec
Mathew Poff <matp@live.ca>
2022-08-31 19:06:18 -0400

    Added timestamp tp status change event UI

--- src/components/issue/IssueEventListItem.tsx
+++ src/components/issue/IssueEventListItem.tsx
@@ -4,20 +4,24 @@ interface IssueEventListItemProps {
   event: IssueEvent;
 }
 export function IssueEventListItem({ event }: IssueEventListItemProps) {
+  const timestamp = (
+    <span title={event.createdAt.toTimeString()}>
+      {event.createdAt.toDateString()}
+    </span>
+  );
+
   return (
     <>
       {event.status && (
         <div className="">
-          (Username) changed the status to {event.status.replaceAll("_", " ")}
+          (Username) changed the status to {event.status.replaceAll("_", " ")}{" "}
+          on {timestamp}
         </div>
       )}
       {event.comment && (
         <div className="rounded-md border">
           <div className="border-b p-2">
-            (Username) commented on{" "}
-            <span title={event.createdAt.toTimeString()}>
-              {event.createdAt.toDateString()}
-            </span>
+            (Username) commented on {timestamp}
           </div>
           <div className="whitespace-pre-line p-2">{event.comment}</div>
         </div>
