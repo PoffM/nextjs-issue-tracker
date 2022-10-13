4f334725a293a704dc7a97d24473f9de7beb2a39
Mathew Poff <matp@live.ca>
2022-10-13 11:42:34 -0400

    Fixed type error

--- src/components/color-mode/useColorMode.ts
+++ src/components/color-mode/useColorMode.ts
@@ -1,12 +1,11 @@
 import useLocalStorage from "@rehooks/local-storage";
 
 export function useColorMode() {
-  const initialColorMode =
-    typeof window === "object"
-      ? window.matchMedia?.("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light"
-      : "dark";
+  const initialColorMode = globalThis.matchMedia
+    ? globalThis.matchMedia?.("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light"
+    : "dark";
 
   const [colorMode, setColorMode] = useLocalStorage(
     "color-mode",
