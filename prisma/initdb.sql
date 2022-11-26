84cc7d0fb0e62dcca97b677c9b3eb967260b72d1
Mathew Poff <matp@live.ca>
2022-11-26 12:01:10 -0500

    Fixed issue where the dev/test DB restarted while Prisma was trying to connect to it when running "npm run dev": Run the init script through "npm run db-up" command instead of through Docker.

--- prisma/initdb.sql
+++ prisma/initdb.sql
@@ -1,3 +1,6 @@
--- Initialize the databases for dev and test:
-create database devdb;
-create database testdb;
+-- Initialize the databases for dev and test.
+-- These should work like "create database if not exists":
+SELECT 'CREATE DATABASE devdb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'devdb')\gexec
+SELECT 'CREATE DATABASE testdb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testdb')\gexec
